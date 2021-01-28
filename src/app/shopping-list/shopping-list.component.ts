5232540d81740a941e2366fe094616b05dd5d493
Houkas <hugor33@outlook.fr>
2021-01-28 20:12:00 +0100

    ajout des composants

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector:'app-shoppingList',
+    templateUrl:'./shopping-list.component.html',
+    styleUrls: ['./shopping-list.component.css'],
+})
+
+export class shoppingListComponent implements OnInit {
+
+    constructor(){
+
+    }
+
+    ngOnInit():void{
+        
+    }
+
+}
